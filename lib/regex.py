07c2fe80162f336286b97c7f04f2fc97abd75307
jemojeremmy2@gmail.com <jemojeremmy2>
2024-05-28 19:35:50 +0300

    Completed regex patterns and tests

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,6 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
-name_regex = re.compile(name)
-
-phone_number = r""
-phone_regex = re.compile(phone_number)
-
-email_address = r""
-email_regex = re.compile(email_address)
+# Define the regular expressions
+name_regex = re.compile(r"^[A-Z][a-zA-Z'-]*( [A-Z][a-zA-Z'-]*)*$")
+phone_regex = re.compile(r"^(\d{10}|\(\d{3}\) \d{3}-\d{4}|\d{3}-\d{3}-\d{4})$")
+email_regex = re.compile(r"^[a-zA-Z][a-zA-Z0-9._]*@[a-zA-Z]+\.[a-zA-Z]+$")
